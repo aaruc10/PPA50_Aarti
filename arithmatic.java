e1f4c0fea7974bfa626743e065f380eee333815e
aaruc10 <145377891+aaruc10@users.noreply.github.com>
2024-05-10 20:57:33 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,13 @@
+package marvellous ;
+
+public class arithmatic
+{
+    public int addition(int a,int b)
+    {
+        return a+b;
+    }
+    public int subtraction(int a,int b)
+    {
+        return a-b;
+    }
+}
